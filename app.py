3154f6d8a17ff4e8853fc50bbcc1969b63f7cc75
Adel Lassag <adetech.aadl@gmail.com>
2019-08-28 17:37:12 +0100

    test: Add user registration

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
 
 # Import modules
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model import Base, User
 from flask import session as login_session
+from flask_uploads import UploadSet, configure_uploads, IMAGES
+
 
 app = Flask(__name__)
+photos = UploadSet('photos', IMAGES)
+
+app.config['UPLOADED_PHOTOS_DEST'] = 'static/img'
+configure_uploads(app, photos)
 
 # Connect to the database
 engine = create_engine('sqlite:///data.db?check_same_thread=False')
@@ -22,10 +28,42 @@ def home():
 
 @app.route('/admin')
 def admin():
-    admin = session.query(User).all()
-    return 'My name is ' + admin[0].name + ' and I\'m ' + admin[0].m_type + ' here!' 
+    users = session.query(User).all()
+    return 'My name is ' + users[0].name + ' and I\'m ' + users[0].m_type + ' here!'
+
+@app.route('/users')
+def display_users():
+    users = session.query(User).all()
+    return render_template('coucou.html', users=users)
 
 
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        filename = photos.save(request.files['file'])
+        password=request.form['password']
+        username = request.form['name']
+        if username is None or password is None:
+            abort(400)
+        user = User(
+            name=username,
+            email=request.form['email'],
+            image=filename,
+            m_type='admin')
+
+        user.hash_password(password)
+        print(user.name, user.password_hash)
+        try:
+            session.add(user)
+            session.commit()
+            print(user.name + ' is added')
+        except:
+            print('Something went wrong')
+        users = session.query(User).all()
+        return (redirect(url_for('display_users')))
+    else:
+        return (render_template('sign-up.html'))
+        
 
 # Run the app in the '__main__' scope
 if __name__ == '__main__':
--- model.py
+++ model.py
@@ -1,8 +1,9 @@
 # Import SQLAlchemy
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, LargeBinary
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
+from passlib.apps import custom_app_context as pwd_context
 
 Base = declarative_base()
 
@@ -11,10 +12,18 @@ class User(Base):
     __tablename__ = 'user'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    name = Column(String(250), nullable=False, index=True)
     email = Column(String(250), nullable=False)
     m_type = Column(String(50), nullable=False)
-    gender = Column(String(50), nullable=False)
+    password_hash = Column(String(64))
+    image = Column(String(1000))
+
+    def hash_password(self, password):
+        self.password_hash = pwd_context.hash(password)
+
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
+
 
     @property
     def serialize(self):
@@ -23,11 +32,41 @@ class User(Base):
             'name': self.name,
             'email': self.email,
             'm_type': self.m_type,
-            'gender': self.gender,
+            'password_hash': self.password_hash,
+            'image': self.image
         }
 
 
+class Category(Base):
+    __tablename__ = 'category'
 
+    id = Column(Integer, primary_key=True)
+    name = Column(String(20), nullable=False)
+    image = Column(String(100))
+    # image = image_attachment('Picture')
+    description = Column(String(2000))
+
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    # Method for API enpoints
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'image': self.image,
+            'description': self.description,
+            'user_id': self.user_id
+        }
+
+'''
+class UserPicture(Base, Image):
+    __tablename__ = 'picture'
+
+    user_id = Column(Integer, ForeignKey('category.id'), primary_key=True)
+    user = relationship('Category')
+ '''
 
 engine = create_engine('sqlite:///data.db')
 Base.metadata.create_all(engine)
