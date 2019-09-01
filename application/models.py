8a23eaffbb82d2c4c34c71e0dbdeeace3e64b50f
Adel Lassag <adetech.aadl@gmail.com>
2019-09-01 20:29:19 +0100

    feat: Add service model

--- application/models.py
+++ application/models.py
@@ -18,6 +18,7 @@ class User(db.Model, UserMixin):
     # Default Image credit is licensed by CC BY 3.0 "https://www.onlinewebfonts.com/icon/191958"
     image_file = db.Column(db.String(20), nullable=False, default='profile.svg')
     password = db.Column(db.String(64), nullable=False)
+    services = db.relationship('Service', backref='owner', lazy=True)
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
@@ -40,6 +41,30 @@ class Category(db.Model):
     category_name = db.Column(db.String(20), unique=True, nullable=False)
     description = db.Column(db.Text)
     image_file = db.Column(db.String(20), nullable=False, default='profile.svg')
+    services = db.relationship('Service', backref='parent', lazy=True)
+
+    def __repr__(self):
+        return f"Category('{self.category_name}', '{self.id}', '{self.image_file}')"
+
+    # Method for API enpoints
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'category_name': self.category_name,
+            'description': self.description,
+            'image_file': self.image_file,
+            'services': self.services
+        }
+
+
+class Service(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    service_name = db.Column(db.String(20), unique=True, nullable=False)
+    description = db.Column(db.Text)
+    image_file = db.Column(db.String(20), nullable=False, default='profile.svg')
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
 
     def __repr__(self):
         return f"Category('{self.category_name}', '{self.id}', '{self.image_file}')"
@@ -53,4 +78,6 @@ class Category(db.Model):
             'description': self.description,
             'image_file': self.image_file
         }
+
+
 db.create_all()
