0426a828b414ea4849327fac73f6f01179a41711
Adel Lassag <adetech.aadl@gmail.com>
2019-09-19 00:03:31 +0100

    test: Create temp cert and run the app over https to test locating user

--- run.py
+++ run.py
@@ -2,4 +2,4 @@
 from application import app
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, threaded=True)
+    app.run(host='0.0.0.0', debug=True, threaded=True, ssl_context=('cert.pem', 'key.pem'))
