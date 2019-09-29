8354fe826d3b9f033175880fec1d68679f75e266
Adel Lassag <adetech.aadl@gmail.com>
2019-09-29 04:05:36 +0100

    deploy: Update requiremenet modules package

--- run.py
+++ run.py
@@ -2,5 +2,6 @@
 from application import app
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, threaded=True, ssl_context=('cert.pem', 'key.pem'))
+    # app.run(host='0.0.0.0', debug=True, threaded=True, ssl_context=('cert.pem', 'key.pem'))
     # app.run(host='192.168.1.4', debug=True, threaded=True)
+    app.run()
